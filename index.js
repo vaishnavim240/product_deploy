fe2f9d550e98d93e5d01b149988efd17196f2586
vaishnavim240 <146851824+vaishnavim240@users.noreply.github.com>
2025-02-03 11:34:10 +0530

    db config

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const productRouter = require('./Routes/ProductRoute')
 app.use(express.json())
 
 app.listen('5000', ()=> console.log('server running on 5000'))
-mongoose.connect('mongodb://localhost:27017/sample')
+mongoose.connect('mongodb+srv://vaishu12092004:vaishnavi12@mycluster.q3thd.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster')
     .then(()=> console.log('database connected..'))
     .catch((err) => console.log(err))
 
 app.use("", productRouter);
 
 console.log("updated");
-// app.get('/', (req, res)=>{
-//     res.send('server reacted...')
-// })
+app.get('/', (req, res)=>{
+    res.send('server reacted...')
+})
